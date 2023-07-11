86f7603fbfc874baa6f17e089b0b94467847dc1f
codyallison <cody.allison26@gmail.com>
2023-07-10 22:02:44 -0500

    flask app can successfully pull the mongo collection data

--- code/app.py
+++ code/app.py
@@ -1,3 +1,4 @@
+from pydoc import doc
 from flask import Flask, jsonify
 import json
 from pymongo import MongoClient
@@ -6,7 +7,8 @@ from pymongo import MongoClient
 
 #mongodb
 mongo = MongoClient(port=27017) 
-db = mongo['healthandgreenspacedata']
+db = mongo.met
+healthandgreenspacedata = db['healthandgreenspacedata']
 
 #################################################
 # Flask Setup
@@ -15,8 +17,14 @@ app = Flask(__name__)
 
 @app.route("/api/health-green-data")
 def health_green_data():
-    
-    return jsonify(db)
+    cursor = healthandgreenspacedata.find({})
+    documents ={}
+    pymongo_cursor = db.healthandgreenspacedata.find({}, {'_id': False})
+    all_data = list(pymongo_cursor)
+    print(all_data)
+    documents['data'] = all_data
+    documents = jsonify(documents)
+    return(documents)
 
 if __name__ == "__main__":
     app.run(debug=True)
