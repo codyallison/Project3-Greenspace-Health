180848f6834d9a385cb0f16eb1966778f7432f27
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 <kennysiegler@Kennys-MacBook-Pro.local>
2023-07-11 19:43:03 -0500

    Started map creation funciton

--- html-js/logic.js
+++ html-js/logic.js
@@ -3,3 +3,9 @@ url = 'http://127.0.0.1:5000/api/health-green-data'
 d3.json(url).then(function(data) {
     console.log(data)
 })
+
+function getLocation(x) {
+    
+    console.log(x.split('-'))
+
+}
